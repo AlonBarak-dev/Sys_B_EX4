595fb989b8290429ef56255b24f4fe7e7ec9acd9
Alon Barak <alonbarak3110@gmail.com>
2022-04-28 16:15:39 +0300

    update demo

--- Demo.cpp
+++ Demo.cpp
@@ -13,6 +13,8 @@
 #include "Contessa.hpp"
 #include "Game.hpp"
 
+#include <exception>
+
 using namespace coup;
 
 #include <iostream>
@@ -31,9 +33,9 @@ int main() {
 	Ambassador ambassador{game_1, "Meirav"};
 	Captain captain{game_1, "Reut"};
 	Contessa contessa{game_1, "Gilad"};
-	
 
 	vector<string> players = game_1.players();
+
 	/*
 		prints:
 		Moshe
@@ -58,24 +60,33 @@ int main() {
 
 	// throws exception, it is duke's turn now
 	try{
-	assassin.income();
+		assassin.income();
+	}catch (const std::exception &e){
+		std::cerr << e.what() << '\n';
 	}
-	catch(exception e){
-	}
-
 	duke.income();
 	assassin.foreign_aid();
 
 	// throws exception, the last operation duke performed
 	// is income, which cannot be blocked by any role
-	captain.block(duke);
+	try{
+		captain.block(duke);
+	}catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
 	cout << duke.coins() << endl; // prints 2
 	cout << assassin.coins() << endl; // prints 3
 
-	// throws exception, the last operation duke performed
+	// throws exception, the last operation assassin performed
 	// is foreign aid, which cannot be blocked by contessa
-	contessa.block(assassin);
+	try{
+		contessa.block(assassin);
+	}catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
 	duke.block(assassin);
 	cout << assassin.coins() << endl; // prints 1
